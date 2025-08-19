369363438c2fae97c727a4055ae22af815e35cab
jaxwong <wongzhengwei2003@gmail.com>
2025-08-19 09:38:28 +0800

    feat: Level 0

---
+++ src/main/java/GbTheFatBoy.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class GbTheFatBoy {
+
+    public static final String NAME = "GbTheFatBoy";
+    public static final String LINE = "_".repeat(60);
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
