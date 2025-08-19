8097d4476c7764a7af25824739b49c9ddd729cfe
jaxwong <wongzhengwei2003@gmail.com>
2025-08-19 13:54:43 +0800

    feat: Level-2

--- src/main/java/GbTheFatBoy.java
+++ src/main/java/GbTheFatBoy.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GbTheFatBoy {
 
     public static final String NAME = "GbTheFatBoy";
     public static final String MESSAGE = "I'm smelly meow meow!";
     public static final String LINE = "_".repeat(60);
+    public static final ArrayList<String> taskList = new ArrayList<String>();
 
     public static void main(String[] args) {
         greet();
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        while (!command.equals("bye")) {
-            echo(command);
-            command = scanner.nextLine();
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) break;
+            else if (command.equals("list")) printList(taskList);
+            else {
+                taskList.add(command);
+                echo(command);
+            }
         }
         bye();
 
@@ -28,13 +35,22 @@ public class GbTheFatBoy {
 
     public static void echo(String command) {
         System.out.println(LINE);
-        System.out.println(command);
+        System.out.println("added: " + command);
         System.out.println(LINE);
     }
 
     public static void bye() {
         System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    public static void printList(ArrayList<String> taskList) {
+        System.out.println(LINE);
+        int length = taskList.size();
+        for (int i = 0; i < length; i++) {
+            System.out.println((i+1) + ". " + taskList.get(i));
+        }
         System.out.println(LINE);
     }
 }
