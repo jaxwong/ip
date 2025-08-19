e5b09c8842e75bf1beb271f7742332c847a20385
jaxwong <wongzhengwei2003@gmail.com>
2025-08-19 09:56:29 +0800

    feat: Level-1

--- src/main/java/GbTheFatBoy.java
+++ src/main/java/GbTheFatBoy.java
@@ -1,16 +1,40 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class GbTheFatBoy {
 
     public static final String NAME = "GbTheFatBoy";
+    public static final String MESSAGE = "I'm smelly meow meow!";
     public static final String LINE = "_".repeat(60);
 
     public static void main(String[] args) {
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            echo(command);
+            command = scanner.nextLine();
+        }
+        bye();
+
+    }
+
+    public static void greet() {
         System.out.println(LINE);
         System.out.println("Hello! I'm " + NAME);
+        System.out.println(MESSAGE);
         System.out.println("What can I do for you?");
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
+    }
 
+    public static void echo(String command) {
+        System.out.println(LINE);
+        System.out.println(command);
+        System.out.println(LINE);
+    }
+
+    public static void bye() {
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
